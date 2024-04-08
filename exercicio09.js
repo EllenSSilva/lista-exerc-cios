8576f9963bfd066ea033a3c35c25f1124c4de772
Ellen Silva <137661449+EllenSSilva@users.noreply.github.com>
2024-04-08 01:33:55 +0000

    exercicio09.js

---
+++ exercicio09.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync') ();
+
+const numero1 = Number (prompt("Qual o primeiro valor?"));
+const numero2 = Number (prompt("Qual o segundo valor?"));
+
+soma= numero1 + numero2;
+mutiplica= soma * numero1;
+
+console.log("soma: ", soma);
+console.log("mutiplica: ", mutiplica);
